adcbd7a4697a9f4fed383886d7dbb9cdeef2b40d
Brett Longworth <blongworth@whoi.edu>
2022-03-23 10:03:00 -0400

    Minor cleanup.

--- read_quantos_files.R
+++ read_quantos_files.R
@@ -10,16 +10,15 @@ library(dplyr)
 
 
 # Quantos data directory path
-dir_path <- "H:/Quantos/quantos/"
+dir_path <- "H:/Quantos/quantos"
 
 files <- dir(path=dir_path, recursive = TRUE, full.names=TRUE, pattern = "*.xls")
-
 
 # Use possibly() to replace bad files with NULL, then compact() to remove nulls
 # This produces a bunch of warnings about data types. Suspect weird data in some spreadsheets
 quantos_data <- files |>
   map(possibly(read_xls, otherwise = NULL),
-      col_names=c("timestamp", "pos", "mass", "valid"), 
+      col_names = c("timestamp", "pos", "mass", "valid"), 
       col_types = c("date", "numeric", "numeric", "text")) |> 
   compact() |>
   reduce(bind_rows)
